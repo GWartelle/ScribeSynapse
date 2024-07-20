2b4a94cff7aada47ab5b5e17c6542e6490b7c329
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-20 19:57:14 +0200

    Mongoose and .env setup

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
+});
+
+mongoose.connect(process.env.MONGO_DB_URI).then(() => {
+  console.log("Connected to MongoDB");
 });
