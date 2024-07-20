43f026ca406f704506fd13f3b253b781f9a56d5f
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-20 17:57:37 +0200

    Header logo setup

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,18 @@
+import { Navbar } from "flowbite-react";
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        Scribe
+        <span className="px-2 pb-1 bg-gradient-to-r from-cyan-500 via-indigo-500 to-pink-500 rounded-lg text-white">
+          Synapse
+        </span>
+      </Link>
+    </Navbar>
+  );
 }
