0298d64058a74eb9f74ea759e339926045f2de7e
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-21 14:01:56 +0200

    MongoDB connection update

--- api/index.js
+++ api/index.js
@@ -9,9 +9,14 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.listen(PORT, () => {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port ${PORT}`);
 });
 
-mongoose.connect(process.env.MONGO_DB_URI).then(() => {
-  console.log("Connected to MongoDB");
-});
+mongoose
+  .connect(process.env.MONGO_DB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
