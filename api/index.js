ab0dc241b6b2bb544c8c986924102cf3a84d0ff8
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-21 23:49:30 +0200

    Auth Routing setup

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.listen(PORT, () => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGO_DB_URI)
