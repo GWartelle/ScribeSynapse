99afab3de19918e9beeb1d150f55dcfd3a227811
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-26 00:37:07 +0200

    Footer section update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,19 @@
 import { Footer } from "flowbite-react";
 import { Link } from "react-router-dom";
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsGithub,
+  BsDribbble,
+} from "react-icons/bs";
 
 export default function FooterComponent() {
   return (
     <Footer container className="border border-t-8 border-cyan-500">
-      <div>
-        <div>
-          <div>
+      <div className="w-full max-w-7xl mx-auto">
+        <div className=" grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
             <Link
               to="/"
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
@@ -17,17 +24,67 @@ export default function FooterComponent() {
               </span>
             </Link>
           </div>
-          <div className="grid grid-cols-2 gap-3 sm:mt-4 sm:grid-cols-3 sm:gap-6">
-            <Footer.Title title="About" />
-            <Footer.LinkGroup col>
-              <Footer.Link
-                href="https://github.com/GWartelle/ScribeSynapse"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Github of this project
-              </Footer.Link>
-            </Footer.LinkGroup>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/GWartelle/ScribeSynapse"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link
+                  href="/about"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  ScribeSynapse
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Contact" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/GWartelle"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  My Github
+                </Footer.Link>
+                <Footer.Link
+                  href="https://www.linkedin.com/in/gabriel-wartelle/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  My LinkedIn
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="ScribeSynapse"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon href="#" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsDribbble} />
           </div>
         </div>
       </div>
