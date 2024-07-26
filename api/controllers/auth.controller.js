e2620504572f6c42b32733e80bad54caff4037c0
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-26 02:16:41 +0200

    Signin controller update

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -14,7 +14,7 @@ export const signup = async (req, res, next) => {
     email === "" ||
     password === ""
   ) {
-    next(errorHandler(400, "All fields are required"));
+    return next(errorHandler(400, "All fields are required"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -37,27 +37,30 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password || email === "" || password === "") {
-    next(errorHandler(400, "All fields are required"));
+    return next(errorHandler(400, "All fields are required"));
   }
 
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) {
-      next(errorHandler(404, "User not found"));
-    }
+    const validPassword = await bcryptjs.compare(
+      password,
+      validUser?.password || ""
+    );
 
-    const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) {
-      next(errorHandler(400, "Invalid password"));
+    if (!validUser || !validPassword) {
+      return next(errorHandler(400, "Invalid email or password"));
     }
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc;
+
     res
       .status(200)
       .cookie("access_token", token, {
         httpOnly: true,
       })
-      .json(validUser);
+      .json(rest);
   } catch (err) {
     next(err);
   }
