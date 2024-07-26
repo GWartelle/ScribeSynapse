aeef69dc37d0d3ab855643228f7b7716559a3e05
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-27 00:05:18 +0200

    OAuth functionnality setup

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,8 +1,41 @@
 import { Button } from "flowbite-react";
 import { AiFillGoogleCircle } from "react-icons/ai";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+import { app } from "./../firebase";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { signInSuccess } from "../redux/user/userSlice";
 
 export default function OAuth() {
-  const handleGoogleClick = async () => {};
+  const auth = getAuth(app);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleGoogleClick = async () => {
+    const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: "select_account" });
+
+    try {
+      const resultsFromGoogle = await signInWithPopup(auth, provider);
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: resultsFromGoogle.user.displayName,
+          email: resultsFromGoogle.user.email,
+          googlePhotoUrl: resultsFromGoogle.user.photoURL,
+        }),
+      });
+
+      const data = await res.json();
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <Button
       type="button"
