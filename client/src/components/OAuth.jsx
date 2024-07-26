bac9ac6e748b3d6422c7a0eeb41fbc705de31b2a
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-26 23:40:00 +0200

    OAuth button setup

---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,17 @@
+import { Button } from "flowbite-react";
+import { AiFillGoogleCircle } from "react-icons/ai";
+
+export default function OAuth() {
+  const handleGoogleClick = async () => {};
+  return (
+    <Button
+      type="button"
+      gradientDuoTone="pinkToOrange"
+      outline
+      onClick={handleGoogleClick}
+    >
+      <AiFillGoogleCircle className="w-6 h-6 mr-2" />
+      <span className="flex items-center">Continue with Google</span>
+    </Button>
+  );
+}
