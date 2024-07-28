ba1bd708d2caeca7473893fd72063a4c9e57d3ca
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-28 19:41:49 +0200

    Dash Profile component update

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,11 +1,12 @@
 import { Button, TextInput } from "flowbite-react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef();
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -15,14 +16,21 @@ export default function DashProfile() {
     }
   };
 
-  console.log(imageFile, imageFileUrl);
-
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
-        <input type="file" accept="image/*" onChange={handleImageChange} />
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+          onClick={() => filePickerRef.current.click()}
+        >
           <img
             src={imageFileUrl || currentUser.profilePicture}
             alt="User profile picture"
