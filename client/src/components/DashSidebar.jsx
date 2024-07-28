d0d14acd58451d922895cc64c7fd4cbeb7230b2f
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-28 18:03:51 +0200

    Dashboard page setup

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,5 @@
+export default function DashSidebar() {
+  return (
+    <div>DashSidebar</div>
+  )
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    console.log(tabFromUrl);
+  }, [location.search]);
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      <div>
+        {/* SIDEBAR */}
+        <DashSidebar />
+      </div>
+      {/* PROFILE */}
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
 }
